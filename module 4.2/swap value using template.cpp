9c911fa5268f2edffac50603e83de09d88ff76d5
kinjalhingrajiya <125260636+kinjalhingrajiya@users.noreply.github.com>
2023-04-14 14:56:04 +0530

    j

--- module 4.2/swap value using template.cpp
+++ module 4.2/swap value using template.cpp
@@ -1,20 +1,19 @@
-#include<iostream>
-using namespace std;
-template <class k>
-int Swap(k& x,k& y)
+#include<iostream>				//header file
+using namespace std;			//standard library
+template <class k>				//template 
+int Swap(k& x,k& y)				//function
 	{
-		k t;
-		x=x+y;
+		k t;					//class object
+		x=x+y;					//Equation for swap value
 		y=x-y;
 		x=x-y;
 		return 0;
-		
 	}
-main()
+main()							//inbuilt function
 {	
-	int x,y;
-	 x=10,y=20;
-	Swap(x,y);
-	cout<<"x="<<x<<endl;
-	cout<<"y="<<y;
+	int x,y;					//variable declaration
+	 x=10,y=20;					//assign value of variable
+	Swap(x,y);					//function calling
+	cout<<"x="<<x<<endl;		//print value of x
+	cout<<"y="<<y;				//print value of y
 }
